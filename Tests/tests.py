f4b232bd1371e43f2e1733d7dfb29debd4e74112
Moises Muiña <mochyfm@gmail.com>
2022-06-07 10:47:34 +0100

    Realizando la prueba de asignacion de valores por defecto (fallida)

--- Tests/tests.py
+++ Tests/tests.py
@@ -25,3 +25,18 @@ class TestContador(unittest.TestCase):
         self.assertEqual(test_funcionamiento.valor_inicial, inicial)
         self.assertEqual(test_funcionamiento.incremento, incremento)
         self.assertEqual(test_funcionamiento.limite, limite)
+
+    def test_valores_default(self):
+
+        """ Declaramos la variable que utilizaremos para realizar la comprobación """
+
+        limite = 5
+
+        """ Creamos el Constructor asignando únicamente el valor del límite """
+
+        test_valores_default = Contador(limite=limite) 
+
+        """ Verificamos que los valores que no han sido asignados tienen los valores esperados """
+
+        self.assertEqual(test_valores_default.valor_inicial, 0)
+        self.assertEqual(test_valores_default.incremento, 1)    
