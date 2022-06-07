accbd9797a4421eeccb79ca6722ce7edf9b41bb8
Moises Muiña <mochyfm@gmail.com>
2022-06-07 10:44:53 +0100

    Se ha arreglado el código para que pueda pasar la prueba anterior

---
+++ Contador/Contador.py
@@ -0,0 +1,7 @@
+class Contador:
+
+    def __init__(self, valor_inicial, incremento, limite):
+        self.valor_inicial = valor_inicial
+        self.incremento = incremento
+        self.limite = limite
+        self.valor_actual = valor_inicial
