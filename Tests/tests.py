162d7168fd4c2ac499fa44f90a87eae67fb298ea
Moises Muiña <mochyfm@gmail.com>
2022-06-07 10:58:40 +0100

    Realizando la prueba de igualacion de valores (fallida)

--- Tests/tests.py
+++ Tests/tests.py
@@ -39,4 +39,30 @@ class TestContador(unittest.TestCase):
         """ Verificamos que los valores que no han sido asignados tienen los valores esperados """
 
         self.assertEqual(test_valores_default.valor_inicial, 0)
-        self.assertEqual(test_valores_default.incremento, 1)    
+        self.assertEqual(test_valores_default.incremento, 1)
+
+    def test_igualacion_valores(self):
+
+        """ Declaramos las variables a las que queremos igualar el contador """
+
+        inicial = 0
+        incremento = 2
+        limite = 5
+
+        """ Creamos el Constructor asignando únicamente el valor del límite """
+
+        test_igualacion_valores = Contador(inicial, incremento, limite)
+
+        """ Igualamos los valores y comprobamos posteriormente si NO han sido igualados, 
+            ya que se pide que no sea igual """
+
+        test_igualacion_valores.__valor_inicial = 5
+        test_igualacion_valores.__incremento = 1
+        test_igualacion_valores.__limite = 10
+
+        self.assertNotEqual(test_igualacion_valores.__valor_inicial, 5)
+        self.assertNotEqual(test_igualacion_valores.__incremento, 1)
+        self.assertNotEqual(test_igualacion_valores.__limite, 10)
+        
+    if __name__ == '__main__':
+        unittest.main()
